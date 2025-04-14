06c214a8474d8aef658f8bccd77abf70d3986358
Kyle Box <kbox799@gmail.com>
2025-04-14 16:31:32 -0400

    moved known good state to correct directory

--- ExpenseTracker/src/test/java/learn/finance/repository/UserJdbcRepositoryTest.java
+++ ExpenseTracker/src/test/java/learn/finance/repository/UserJdbcRepositoryTest.java
@@ -1,7 +1,12 @@
 package learn.finance.repository;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class UserJdbcRepositoryTest {
+UserJdbcRepository repository;
+
 
 }
