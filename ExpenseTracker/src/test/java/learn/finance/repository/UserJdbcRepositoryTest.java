af9c982dbac0b595531da4b45edbfe93df260e14
Kyle Box <kbox799@gmail.com>
2025-04-14 16:44:11 -0400

    configured application.properties for tests and main

--- ExpenseTracker/src/test/java/learn/finance/repository/UserJdbcRepositoryTest.java
+++ ExpenseTracker/src/test/java/learn/finance/repository/UserJdbcRepositoryTest.java
@@ -1,7 +1,25 @@
 package learn.finance.repository;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class UserJdbcRepositoryTest {
+
+    @Autowired
+    UserJdbcRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
+
 
 }
