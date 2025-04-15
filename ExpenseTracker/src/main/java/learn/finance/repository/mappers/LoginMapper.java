d366ca8f278678c2ee8c745797d46e8f0cce0083
Kyle Box <kbox799@gmail.com>
2025-04-15 14:51:22 -0400

    revised log to implement users from security, updated loginMapper, updated Repository with relevant changes

--- ExpenseTracker/src/main/java/learn/finance/repository/mappers/LoginMapper.java
+++ ExpenseTracker/src/main/java/learn/finance/repository/mappers/LoginMapper.java
@@ -5,18 +5,28 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginMapper implements RowMapper<Login> {
 
+    private final List<String> roles;
+
+    public LoginMapper(List<String> roles) {
+        this.roles = roles;
+    }
+
+
     @Override
     public Login mapRow(ResultSet resultSet, int i) throws SQLException {
-        Login login = new Login();
-        login.setLoginId(resultSet.getInt("login_id"));
-        login.setUserId(resultSet.getInt("user_id"));
-        login.setUsername(resultSet.getString("user_name"));
-        login.setPassword(resultSet.getString("password"));
-        login.setRoleId(resultSet.getInt("role_id"));
-        login.setDisabled(resultSet.getBoolean("disabled"));
-        return login;
+        return new Login (
+                resultSet.getInt("login_id"),
+                resultSet.getInt("user_id"),
+                resultSet.getString("user_name"),
+                resultSet.getString("password"),
+                resultSet.getInt("role_id"),
+                resultSet.getBoolean("disabled"),
+                roles
+        );
     }
 }
