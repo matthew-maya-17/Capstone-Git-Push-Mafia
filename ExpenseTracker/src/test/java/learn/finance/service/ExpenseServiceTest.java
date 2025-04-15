747a4df82ce18f81f07d8c3fd6dafba5baa1b298
Kyle Box <kbox799@gmail.com>
2025-04-14 23:01:23 -0400

    added 6 tests to expensejdbcrepository test

--- ExpenseTracker/src/test/java/learn/finance/service/ExpenseServiceTest.java
+++ ExpenseTracker/src/test/java/learn/finance/service/ExpenseServiceTest.java
@@ -24,12 +24,56 @@ class ExpenseServiceTest {
     @Test
     void shouldFindAll(){
         List<Expense> expected = List.of(
-                new Expense(1, 1, 1, 100.0, null, LocalDateTime.parse("2025-04-06 06:03:49") , null, false, false, "https://www.example.org/"),
-                new Expense(2, 32, 4, 100.0, null, LocalDateTime.parse("2025-04-06 06:03:49") , null, false, false, "http://www.example.com/#actor")
+                new Expense(1, 1, 1, 100.0, null, LocalDateTime.parse("2025-07-15T14:30:00") , null, false, false, "https://www.example.org/"),
+                new Expense(2, 32, 4, 100.0, null, LocalDateTime.parse("2025-10-15T14:30:00") , null, false, false, "http://www.example.com/#actor")
                 );
         when(repository.findAll()).thenReturn(expected);
         List<Expense> actual = service.findAll();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotAddZeroOrNegativeAmount() {
+        Expense expense = new Expense(1, 1, 1, -100.0, null, LocalDateTime.parse("2025-07-15T14:30:00") , null, false, false, "https://www.example.org/");
+        when(repository.addExpense(expense)).thenReturn(expense);
+        Result<Expense> result = service.addExpense(expense);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotAddMissingReceiptUrl() {
+        Expense expense = new Expense(1, 1, 1, 100.0, null, LocalDateTime.parse("2025-07-15T14:30:00") , null, false, false, null);
+        when(repository.addExpense(expense)).thenReturn(expense);
+        Result<Expense> result = service.addExpense(expense);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldAddValid() {
+        Expense expense = new Expense(0, 1, 1, 100.0, null, LocalDateTime.parse("2025-07-15T14:30:00") , null, false, false, "https://www.example.org/");
+        Expense expected = new Expense(0, 1, 1, 100.0, null, LocalDateTime.parse("2025-07-15T14:30:00") , null, false, false, "https://www.example.org/");
+        when(repository.addExpense(expense)).thenReturn(expected);
+        Result<Expense> result = service.addExpense(expense);
+        assertTrue(result.isSuccess());
+    }
+    @Test
+    void shouldNotUpdateNull() {
+        Expense expense = null;
+        Result<Expense> result = service.updateExpense(expense);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotUpdateNoUserId() {
+        Expense expense = new Expense(0, 0, 1, 100.0, null, LocalDateTime.parse("2025-07-15T14:30:00") , null, false, false, "https://www.example.org/");
+        Result<Expense> result = service.updateExpense(expense);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+    @Test
+    void shouldNotUpdateNoCategoryId() {
+        Expense expense = new Expense(0, 1, 0, 100.0, null, LocalDateTime.parse("2025-07-15T14:30:00") , null, false, false, "https://www.example.org/");
+        Result<Expense> result = service.updateExpense(expense);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
 }
