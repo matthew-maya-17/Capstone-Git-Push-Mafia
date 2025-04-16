9b3c367de0b62f33436496c669a36580d3b2c943
matthew-maya-17 <matthewmatt2009@hotmail.com>
2025-04-15 23:26:58 -0400

    Basic Front End Features working (NavBar onClick events)

--- ExpenseTracker/client/expense-tracker-ui/src/Navbar.js
+++ ExpenseTracker/client/expense-tracker-ui/src/Navbar.js
@@ -1,17 +1,17 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Navbar(){
-    return(<>
-        <nav>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/login'}>Login</Link>
-            <Link to={'/register'}>Register</Link>
-            <Link to={'/expenses'}>Expenses</Link>
-            <Link to={'/expense/add'}>Add An Expense</Link>
-        </nav>
-
-    </>)
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/expense/add"}>Add An Expense</Link>
+        <Link to={"/expense"}>Expenses</Link>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/register"}>Register</Link>
+      </nav>
+    </>
+  );
 }
-
 
 export default Navbar;
