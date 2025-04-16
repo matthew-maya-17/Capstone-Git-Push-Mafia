3e76c5088a373cce4cb6e304e8854c7570ffd883
Kyle Box <kbox799@gmail.com>
2025-04-15 22:41:33 -0400

    corrected navbar name inconsistencies

--- ExpenseTracker/client/expense-tracker-ui/src/Navbar.js
+++ ExpenseTracker/client/expense-tracker-ui/src/Navbar.js
@@ -6,8 +6,8 @@ function Navbar(){
             <Link to={'/'}>Home</Link>
             <Link to={'/login'}>Login</Link>
             <Link to={'/register'}>Register</Link>
-            <Link to={'/agents'}>Agents</Link>
-            <Link to={'/agent/add'}>Add An Agent</Link>
+            <Link to={'/expenses'}>Expenses</Link>
+            <Link to={'/expense/add'}>Add An Expense</Link>
         </nav>
 
     </>)
