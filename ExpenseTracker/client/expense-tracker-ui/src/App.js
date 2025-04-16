fbcc7ebd9f437508bc199ff88bffa4cb986d6a7a
matthew-maya-17 <matthewmatt2009@hotmail.com>
2025-04-15 22:36:42 -0400

    Setup App.js using Router

--- ExpenseTracker/client/expense-tracker-ui/src/App.js
+++ ExpenseTracker/client/expense-tracker-ui/src/App.js
@@ -1,4 +1,24 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import ExpenseForm from "./ExpenseForm";
+import ExpenseList from "./ExpenseList";
+import Home from "./Home";
+import NotFound from "./NotFound";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/expense/add" element={<ExpenseForm />} />
+          <Route path="/expense/edit/:id" element={<ExpenseForm />} />
+          <Route path="/expense" element={<ExpenseList />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 export default App;
